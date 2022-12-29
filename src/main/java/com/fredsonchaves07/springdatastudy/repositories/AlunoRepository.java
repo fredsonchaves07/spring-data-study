ec17a1723d43a30eeecb7c714820bfc292601753
Fredson <fredsonchaves07@gmail.com>
2022-12-29 11:53:32 -0300

    feat: utilizacao de jpa query

--- src/main/java/com/fredsonchaves07/springdatastudy/repositories/AlunoRepository.java
+++ src/main/java/com/fredsonchaves07/springdatastudy/repositories/AlunoRepository.java
@@ -2,10 +2,14 @@ package com.fredsonchaves07.springdatastudy.repositories;
 
 import com.fredsonchaves07.springdatastudy.entities.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.*;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 
     List<Aluno> findbyNome(String nome);
+
+    @Query("SELECT a FROM Aluno a WHERE a.nome like :nome% AND a.idade >= :idade")
+    List<Aluno> findByNomeIdadeIgualOuMaior(String nome, Integer idade);
 }
